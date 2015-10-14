4013066e824921e92f93a002690d2115df301de1
Michael Cohen <budjmt@aol.com>
2015-10-13 20:19:15 -0400

    Fix previous issues, code base, prototype, "enivornment"

--- js/movable.js
+++ js/movable.js
@@ -1,27 +1,25 @@
-function Movable (x, y, mass) 
+var Movable = function(x, y, mass) 
 {
     this.pos = new Vector(x, y);
-
     this.vel = new Vector(0, 0);
-
     this.accel = new Vector(0, 0);
 
     this.mass = mass;
+}
 
-    this.applyForce= function (force, radianDirection)
-    {
+Movable.prototype.applyForce = function (force, radianDirection)
+{
 
-    };
+}
 
-    this.update = function(dt){
-         
-        //this.accel.x = this.force.x / this.mass;
-        //this.accel.y = this.force.y / this.mass;
+Movable.prototype.update = function(dt){
+     
+    //this.accel.x = this.force.x / this.mass;
+    //this.accel.y = this.force.y / this.mass;
 
-        this.vel.x += this.accel.x * dt;
-        this.vel.y += this.accel.y * dt;
+    this.vel.x += this.accel.x * dt;
+    this.vel.y += this.accel.y * dt;
 
-        this.pos.x += this.vel.x * dt;
-        this.pos.y += this.vel.y * dt;
-    };
+    this.pos.x += this.vel.x * dt;
+    this.pos.y += this.vel.y * dt;
 }
--- js/vector.js
+++ js/vector.js
@@ -1,77 +1,71 @@
-function Vector(initX, initY)
+var Vector = function(initX, initY)
 {
 	this.x = initX;
 	this.y = initY;
-	
-	this.getMagSquared= function()
-	{
-		return this.x*this.x+this.y*this.y;
-	};
-	
-	this.getMag= function()
-	{
-		return Math.sqrt(this.getMagSquared());
-	};
-	
-	this.setMag= function(newMag)
-	{
-		var mag = this.getMag();
-		this.x *= newMag/mag;
-		this.y *= newMag/mag;
-	};
-	
-	this.normalize= function()
-	{
-		var mag = this.getMag();
-		return new Vector(this.x/mag, this.y/mag);
-	};
-	
-	this.rotate = function(rot) {
-		var rotVec = this.copy();
-		var c = Math.cos(rot);
-		var s = Math.sin(rot);
-		rotVec.x = c * this.x - s * this.y;
-		rotVec.y = s * this.x + c * this.y;
-		return rotVec;
-	}
-	
-	this.heading = function() {
-		return Math.acos(this.normalize().x);
-	}
-	
-	this.add= function(otherVec)
-	{
-		return new Vector(this.x+otherVec.x, this.y+otherVec.y);
-	}
-	
-	this.sub= function(otherVec)
-	{
-		return new Vector(this.x-otherVec.x, this.y-otherVec.y);
-	}
-	
-	this.mult= function(num)
-	{
-		return new Vector(this.x*num, this.y*num);
-	}
-	
-	this.div= function(num)
-	{
-		return new Vector(this.x/num, this.y/num);
-	}
-	
-	this.dot= function(otherVec)
-	{
-		return this.x*otherVec.x+this.y*otherVec.y;
-	};
-	
-	this.copy = function()
-	{
-		return new Vector(this.x, this.y);
-	};
-	
-	this.toString= function()
-	{
-		return "("+this.x+", "+this.y+")";
-	};
-	
+}
+
+Vector.prototype.mag = function()
+{
+	return Math.sqrt(this.x * this.x + this.y * this.y);
+}
+
+Vector.prototype.setMag = function(newMag)
+{
+	var mag = this.mag();
+	this.x *= newMag / mag;
+	this.y *= newMag / mag;
+}
+
+Vector.prototype.normalize = function()
+{
+	var mag = this.mag();
+	return new Vector(this.x / mag, this.y / mag);
+}
+
+Vector.prototype.rotate = function(rot) {
+	var rotVec = this.copy();
+	var c = Math.cos(rot);
+	var s = Math.sin(rot);
+	rotVec.x = c * this.x - s * this.y;
+	rotVec.y = s * this.x + c * this.y;
+	return rotVec;
+}
+
+Vector.prototype.heading = function() {
+	return Math.acos(this.normalize().x);
+}
+
+Vector.prototype.add = function(otherVec)
+{
+	return new Vector(this.x + otherVec.x, this.y + otherVec.y);
+}
+
+Vector.prototype.sub = function(otherVec)
+{
+	return new Vector(this.x - otherVec.x, this.y - otherVec.y);
+}
+
+Vector.prototype.mult = function(num)
+{
+	return new Vector(this.x * num, this.y * num);
+}
+
+Vector.prototype.div = function(num)
+{
+	return new Vector(this.x / num, this.y / num);
+}
+
+Vector.prototype.dot = function(otherVec)
+{
+	return this.x * otherVec.x + this.y * otherVec.y;
+}
+
+Vector.prototype.copy = function()
+{
+	return new Vector(this.x, this.y);
+}
+
+Vector.prototype.toString = function()
+{
+	return "(" + this.x + ", " + this.y + ")";
 }
