ca3d10661beb1090d55a67e3be9851054307c51f
DigitalSeduction <rpark682@gmail.com>
2015-10-18 12:28:48 -0400

    Messed with tongue lerp back. Eh

--- js/tongue.js
+++ js/tongue.js
@@ -8,9 +8,9 @@ Segment.prototype.length = function() {
 	return this.end.sub(this.start).mag();
 }
 
-Segment.prototype.retract = function() {
+Segment.prototype.retract = function(lerpSpeed) {
 	var slope = (this.end.y - this.start.y) / (this.end.x - this.start.x);
-	this.end = lerpVector(this.end,this.start,0.5);
+	this.end = lerpVector(this.end,this.start,clamp(lerpSpeed,0,1));
 }
 
 var Tongue = function() {
@@ -24,20 +24,24 @@ var Tongue = function() {
 	this.canExtend = true;//you can extend if you aren't retracting
 	this.extending = false;//when you are moving the tongue around
 	
+	this.lerpSum = 0.5;
+	
 	this.mouse = null;//this is so that the new segments aren't weird
 }
 
 Tongue.prototype.update = function(dt) {
 	if(!this.canExtend) {
-		this.currSegment.retract();
+		this.currSegment.retract(this.lerpSum);
 		if(this.currSegment.end.sub(this.currSegment.start).mag() < 0.1) {
 			this.segments.pop();
 			this.currSegment = this.segments[this.segments.length - 1];
 			this.numSegments--;
+			this.lerpSum += 0.035;
 			if(this.numSegments < 1) {
 				this.currSegment = null;
 				this.canExtend = true;
 				this.extending = false;
+				this.lerpSum = 0.5;
 			}
 		}
 	}
@@ -54,6 +58,7 @@ Tongue.prototype.update = function(dt) {
 		}
 		if(this.numSegments >= this.maxSegments) {
 			this.canExtend = false;
+			this.currTotalSegments = this.numSegments;
 			this.currTime = 0;
 		}
 		else {
